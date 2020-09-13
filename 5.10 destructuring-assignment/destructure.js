9627a53c44b621a4352d5fa7611a921d6c5d9bb9
Andrii Vlasenko <vav4819@gmail.com>
2020-09-13 13:42:21 +0300

    solve task #5.10

---
+++ 5.10 destructuring-assignment/destructure.js
@@ -0,0 +1,10 @@
+let user = {
+    name: "John",
+    years: 30
+};
+
+let {name: name, years: age, isAdmin: isAdmin = false} = user;
+
+console.log( name ); // John
+console.log( age ); // 30
+console.log( isAdmin ); // false
